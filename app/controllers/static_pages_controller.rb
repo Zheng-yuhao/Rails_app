12642e0fdb9ca6960f01258c09950546dc3cb803
FUrryUp <hirrochi0211@icloud.com>
2021-09-29 03:37:02 +0000

    rails generate controller stat home help

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+  def home
+    # => "app/views/#{resource name}/@{action name}.html.erb"
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get request
+  get 'static_pages/home' # => StaticPages#home
+  get 'static_pages/help' # => StaticPages#help
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
