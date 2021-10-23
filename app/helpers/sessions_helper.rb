3a1d27d24a8b5d3f7b5a546338e1443d9f969de0
FUrryUp <hirrochi0211@icloud.com>
2021-10-23 10:52:37 +0000

    Finish ch09

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,8 +5,14 @@ module SessionsHelper
   
   # 定义current_user存不存在
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if(user_id=session[:user_id])
+      @current_user ||= User.find_by(id:user_id)
+    elsif(user_id=cookies.signed[:user_id])
+      user = User.find_by(id:user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
@@ -17,9 +23,23 @@ module SessionsHelper
   
   def log_out
     # 和flash.now一样session也有method
+    forget(self.current_user) # 这里可以简略为forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   # 为了确保不同的db有不同的情况
   before_save { self.email = self.email.downcase }
   # 对model写入的情况加限制 这里就是实际写的validation
@@ -17,5 +18,23 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    self.update_attribute(:remember_digest, User.digest(self.remember_token))
+  end  
+  # User的instance variable的话定义在这里的method都能用
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
